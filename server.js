aa5c92780617ecc15178b3badd00f50d5baed3a3
Becca K <rkopstein@gmail.com>
2018-07-12 23:20:31 -0400

    added to EVRYTHG

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 8080;
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
