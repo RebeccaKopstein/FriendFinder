66fd32859b61951d3f1d9f3cf54c1ddf080562b8
Becca K <rkopstein@gmail.com>
2018-08-06 20:50:42 -0400

    made corrections

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,48 @@
+var friendData = require('../data/friends.js');
+var path = require('path');
+
+var totalDifference = 0;
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+	
+	
+
+    app.post('/api/friends', function(req, res){
+console.log(req.body)
+// 		var awesomeMatch = {
+// 			name: "",
+// 			image: "",
+// 			matchDifference: 1000
+// 		};
+// 		var userData 	= req.body;
+// 		var userName 	= userData.name;
+// 		var userImage 	= userData.image;
+// 		var userScores 	= userData.scores;
+
+       
+        
+//         for(var i = 0; i < [friends].length-1; i++){
+// 			console.log(friends[i].name);
+// 			totalDifference = 0;
+
+//             for(var j = 0; j < 10; j++){
+//                 totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+// 				if (totalDifference <= awesomeMatch.friendDifference){
+//                     awesomeMatch.name = friends[i].name;
+// 					awesomeMatch.photo = friends[i].photo;
+// 					awesomeMatch.matchDifference = totalDifference;
+// 				}
+// 			}
+// 		}
+
+
+//         friends.push(userData);
+//         friends.push(userName);
+//         friends.push(userImage);
+ 
+// 		res.json(awesomeMatch);
+	});
+};
--- server.js
+++ server.js
@@ -8,13 +8,11 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 bodyParser(); 
-path();
+// path();
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./app/routing/apiRoutes')(app); 
+require('./app/routing/htmlRoutes')(app);
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
